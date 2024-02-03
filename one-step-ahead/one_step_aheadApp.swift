c1d85ff4026172e5bc70e2a3b628117d34b650dd
Michelle Lin <michellelin810@gmail.com>
2024-02-02 16:06:44 -0800

    Initial Commit

---
+++ one-step-ahead/one_step_aheadApp.swift
@@ -0,0 +1,17 @@
+//
+//  one_step_aheadApp.swift
+//  one-step-ahead
+//
+//  Created by michelle on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct one_step_aheadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ one-step-aheadUITests/one_step_aheadUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  one_step_aheadUITestsLaunchTests.swift
+//  one-step-aheadUITests
+//
+//  Created by michelle on 2/2/24.
+//
+
+import XCTest
+
+final class one_step_aheadUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
