dfa1ff6c8ea759da3ce323638fa49ef66d477cda
Meggie Nguyen <meggien1@uci.edu>
2024-02-18 23:36:17 -0800

    made the buttons work, added recommendation title, fixed alignment

--- one-step-ahead/Views/DashboardView.swift
+++ one-step-ahead/Views/DashboardView.swift
@@ -15,71 +15,73 @@ struct DashboardView: View {
     @State private var showSleepView = false
     
     var body: some View {
-        NavigationStack {
-            // TODO: Fetch actual name
-            VStack(alignment: .leading) {
-                Text("Welcome back, Michelle!")
+        NavigationView{
+            VStack {
+                VStack(alignment: .leading) {
+                    // TODO: Fetch actual name
+                    Text("Welcome back, Michelle!")
+                        .font(.system(size: 30))
+                        .padding()
+                }
+                
+                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())]) {
+                    
+                    NavigationLink(destination: ExerciseView()) {
+                        buttonContent(imageName: "figure.walk.circle", text: "exercise activity", backgroundColor: buttonColors[0])
+                    }
+                    NavigationLink(destination: WaterView()) {
+                        buttonContent(imageName: "drop.circle", text: "water intake", backgroundColor: buttonColors[1])
+                    }
+                    NavigationLink(destination: SleepView()) {
+                        buttonContent(imageName: "moon.circle", text: "sleep patterns", backgroundColor: buttonColors[2])
+                    }
+                    NavigationLink(destination: HealthKitView()) {
+                        buttonContent(imageName: "person.circle", text: "profile", backgroundColor: buttonColors[3])
+                    }
+                    .padding()
+                }
+                .padding()
+                
+                VStack(alignment: .trailing) {
+                    Text("Recommendations")
+                    .font(.system(size: 30))
+                }
+                Spacer()
+            }
+            .navigationTitle("Dashboard")
+            
+            VStack(alignment: .trailing) {
+                // TODO: Fetch actual name
+                Text("Recommendations")
                     .font(.system(size: 30))
                     .padding()
             }
-            VStack {
-                // TODO: Link button to page
-                LazyVGrid(columns: columns, spacing: 20) {
-                    Button(action: {
-                        // Action for the first button
-                        print("First button tapped")
-                    }) {
-                        buttonContent(imageName: "figure.walk.circle", text: "exercise activity", backgroundColor: buttonColors[0])
-                    }
-                    .navigationDestination(isPresented: $showExerciseView) {ExerciseView()}
-                    
-                    Button(action: {
-                        print("Second button tapped")
-                    }) {
-                        buttonContent(imageName: "drop.circle", text: "water intake", backgroundColor: buttonColors[1])
-                    }
-                    .navigationDestination(isPresented: $showExerciseView) {WaterView()}
-                    
-                    Button(action: {
-                        // Action for the third button
-                        print("Third button tapped")
-                    }) {
-                        buttonContent(imageName: "moon.circle", text: "sleep patterns", backgroundColor: buttonColors[2])
-                    }
-                    .navigationDestination(isPresented: $showWaterView) {SleepView()}
-                    
-                    Button(action: {
-                        // Action for the fourth button
-                        print("Fourth button tapped")
-                    }) {
-                        buttonContent(imageName: "square.and.pencil.circle", text: "log my activity", backgroundColor: buttonColors[3])
-                    }
-                    .navigationDestination(isPresented: $showExerciseView) {ExerciseView()}
-                }
-                .padding()
-            }
+
         }
     }
     
-    private func buttonContent(imageName: String, text: String, backgroundColor: Color) -> some View {
-        VStack {
-            Image(systemName: imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 100, height: 100)
-                .padding(.horizontal, 10)
-                .foregroundColor(Color.white) // color of the icons
-            
-            Text(text)
-                .foregroundColor(Color.white)
-                .padding(.horizontal, 2)
-        }
-        .frame(width: 132, height: 150)
-        .padding()
-        .background(backgroundColor) // Customize the background color if needed
-        .cornerRadius(10)
-    }
 }
+                    
+private func buttonContent(imageName: String, text: String, backgroundColor: Color) -> some View {
+    VStack {
+        Image(systemName: imageName)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 100, height: 100)
+            .padding(.horizontal, 10)
+            .foregroundColor(Color.white) // color of the icons
+        
+        Text(text)
+            .foregroundColor(Color.white)
+            .padding(.horizontal, 2)
+    }
+    .frame(width: 132, height: 150)
+    .padding()
+    .background(backgroundColor) // Customize the background color if needed
+    .cornerRadius(10)
+}
+
+
 
 #Preview {
     DashboardView()
