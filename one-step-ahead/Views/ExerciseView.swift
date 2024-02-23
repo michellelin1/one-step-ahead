befeb6897f07cc49695dd772ed595438affd41de
Meggie Nguyen <meggien1@uci.edu>
2024-02-22 18:59:37 -0800

    fixed calories burned on exercise

--- one-step-ahead/Views/ExerciseView.swift
+++ one-step-ahead/Views/ExerciseView.swift
@@ -10,16 +10,19 @@ import SwiftUI
 struct ExerciseView: View {
     @ObservedObject var recommendationViewModel = RecommendationViewModel()
     @EnvironmentObject var authHandler: AuthViewModel
+    @ObservedObject var healthKitViewModel = HealthKitViewModel()
     
     var body: some View {
         VStack{
             Text("Exercise Recommendation: \(recommendationViewModel.calorieRecommendation)")
             Text("Exercise history [0]: \(recommendationViewModel.getExerciseHistory().count)")
-            Text("Current calories burned: \(recommendationViewModel.getCurrentCaloriesBurned())")
+            Text("Current calories burned: \(healthKitViewModel.formattedCalBurned())")
 //            Text("Exercise history [1]: \(recommendationViewModel.getExerciseHistory()[1].caloriesBurned)")
+//            String(format: "%.1f calories", caloriesBurned)
         }.onAppear {
             recommendationViewModel.setUser(authHandler.user ?? User.empty)
             recommendationViewModel.getCaloriesRecommendation()
+            healthKitViewModel.fetchCaloriesBurned()
         }
     }
 }
