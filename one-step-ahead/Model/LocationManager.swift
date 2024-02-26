eaaad90f8c32110cc1378a07aa9ca31f09c890fd
Meggie Nguyen <meggien1@uci.edu>
2024-02-26 01:06:25 -0800

    added weather api

---
+++ one-step-ahead/Model/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  one-step-ahead
+//
+//  Created by Meggie Nguyen on 2/25/24.
+//
+
+import Foundation
---
+++ one-step-ahead/Model/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  one-step-ahead
+//
+//  Created by Meggie Nguyen on 2/25/24.
+//
+
+import Foundation
