ce60a64a7d550dc33378066366bf4fbbfae5562b
Meggie Nguyen <meggien1@uci.edu>
2024-02-26 23:35:59 -0800

    revert

--- one-step-ahead/Model/LocationManager.swift
+++ one-step-ahead/Model/LocationManager.swift
@@ -2,38 +2,7 @@
 //  LocationManager.swift
 //  one-step-ahead
 //
-//  Created by Meggie Nguyen on 2/25/24.
+//  Created by Meggie Nguyen on 2/26/24.
 //
 
 import Foundation
-import CoreLocation
-
-class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
-    let manager = CLLocationManager()
-    
-    // create location and loading state
-    @Published var location: CLLocationCoordinate2D?
-    @Published var isLoading = false
-    
-    override init() {
-        super.init()
-        manager.delegate = self
-        manager.requestWhenInUseAuthorization()
-        manager.startUpdatingLocation()
-    }
-    
-    func requestLocation() {
-        isLoading = true
-        manager.requestLocation()
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        location = locations.first?.coordinate
-        isLoading = false
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Error getting location", error)
-        isLoading = false
-    }
-}
